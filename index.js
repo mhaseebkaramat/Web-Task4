33d42eff0344f91dc1c4ac4527fd9f45cf59999a
muhammadhaseeb <chhaseebofficial@gmail.com>
2024-01-17 16:54:56 +0500

    files created

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-function formSubmit() {
-    // Get form data
+// code to check form validation
+function formValidation(){
     const fname = document.getElementById("fname").value;
     const lname = document.getElementById("lname").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-  
-    // Create a new table row
-    const table = document.getElementById("user_table");
-    const newRow = table.insertRow();
-  
-    // Create cells and fill them with data
-    const fnameCell = newRow.insertCell();
-    fnameCell.textContent = fname;
-  
-    const lnameCell = newRow.insertCell();
-    lnameCell.textContent = lname;
-  
-    const emailCell = newRow.insertCell();
-    emailCell.textContent = email;
+    if(fname==""){
+      alert("Please enter user name");
+    }
+    else if(lname==""){
+      alert("Please enter last name");
+    }
+    else if(email==""){
+      alert("Please enter user email");
+    }
+    else if(!email.includes("@gmail.com")){
+      alert("Please enter correct email e.g: abc@gmail.com");
+    }
+    else if(password==""){
+      alert("Please enter your password");
+    }
+    else{
+      formSubmit();
+    }
+}
 
-    const passwordCell = newRow.insertCell();
-    passwordCell.textContent = password;
+function formSubmit() {
   
+    // Get values from data
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // Create a new table row
+    let table = `<tr id="table_row">
+                   <td>${fname}</td>
+                   <td>${lname}</td>
+                   <td><a>${email}</a></td>
+                   <td>${password}</td>
+                   <td><button>Delete</button></td>
+                   </tr>`
+        let tab = document.getElementById("tbl").innerHTML += table;
+        
+       //delete table row
+       let index, mytable = document.getElementById("user_table");
+        for(let i=1; i<mytable.rows.length; i++){
+          mytable.rows[i].cells[4].onclick = function(){
+            index = this.parentElement.rowIndex;
+            mytable.deleteRow(index);
+            console.log(index);
+          }
+        }
+        
     // Clear form inputs
     document.getElementById("registration_form").reset();
   }
-  
-
-
-  function filterTable(event) {
-    const filterValue = event.target.value.toLowerCase();
-    const table = document.getElementById("user_table");
-    const tr = table.querySelectorAll("tbody tr");
-  
-    for (let i = 0; i < tr.length; i++) {
-      const td = tr[i].querySelectorAll("td");
-      let found = false;
-  
-      for (let j = 0; j < td.length; j++) {
-        const cellText = td[j].textContent.toLowerCase();
-        if (cellText.includes(filterValue)) {
-          found = true;
-          break;
-        }
-      }
-  
-      tr[i].style.display = found ? "" : "none";
-    }
-  }
-  
+ 
